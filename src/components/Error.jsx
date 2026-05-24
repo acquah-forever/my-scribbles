e9811cd2bb6516eb129b7103444d5814df6b45f9
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-05-24 15:48:07 +0000

    finalised error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>
-        <span className='text-4xl'>404 Error</span>
-        <h1>Page Not Found</h1>
-        <button>Back Home</button>
+    <div className='space-y-8 flex flex-col justify-center items-center'>
+        <h1 className='text-5xl text-red-500 uppercase'>404 Error Page</h1>
+        <h1 className='uppercase text-2xl'>Page Not Found</h1>
+        <button className='bg-slate-400 p-4 cursor-pointer rounded hover:scale-105 transition-all duration-150'>Back to Homepage</button>
     
     </div>
   )
